34386ea3656e1d3c613baa439baa8660b980b237
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-03-01 20:13:50 +0300

    Add files via upload
    HW8

---
+++ Homeworks 2024/SEMINARs8/__init__.py
@@ -0,0 +1,11 @@
+from HW8_1 import *
+from Seminar8_1 import *
+from Seminar8_2 import *
+from Seminar8_3 import *
+from Seminar8_4 import *
+from Seminar8_5 import *
+from Seminar8_6 import *
+
+
+csv_to_json()
+
